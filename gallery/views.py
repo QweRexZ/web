c1eaf8d8c0c8915f98c58d81bfdcf7b1c7fe125f
SAPR <SAPR@PC-NXT>
2026-02-04 14:21:58 +0700

    Week 1: Added templates and mock data loop

--- gallery/views.py
+++ gallery/views.py
@@ -1,5 +1,24 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 
+#def home(request):
+    #return HttpResponse("<h1>Добро Пожаловать в 3D хранилище</h1><p>Система работает .</p>")
+
+def about(request):
+    return HttpResponse("<h1>Курс WEB структуры.</h1>")
+
 def home(request):
-    return HttpResponse("<h1>Добро Пожаловать в 3D хранилище</h1><p>Система работает .</p>")
+
+    fake_database = [
+        {'id': 1, 'name': 'Sci-Fi Helmet', 'file_size': '15 MB'},
+        {'id': 2, 'name': 'Old Chair', 'file_size': '2 MB'},
+        {'id': 3, 'name': 'Cyber Truck', 'file_size': '10 MB'},
+        {'id': 4, 'name': 'Plane', 'file_size': '12 MB'},
+    ]
+
+    context_data = {
+        'page_title': 'Главная Галерея',
+        'assets': fake_database,
+    }
+    
+    return render(request, 'gallery/index.html', context_data)
