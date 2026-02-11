03bf7b9f2f5c4af9f8f0ce49df5edb24a3f9956c
SAPR <SAPR@PC-NXT>
2026-02-11 14:48:20 +0700

    1

--- config/urls.py
+++ config/urls.py
@@ -18,8 +18,17 @@ from django.contrib import admin
 from django.urls import path
 from gallery.views import home
 from gallery.views import about
+from gallery.views import upload
+
+from django.conf import settings
+from django.conf.urls.static import static
+
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('',home, name='home'),
     path('about/', about , name = 'about'),
+    path('upload/',upload, name='upload_asset'),
 ]
+
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
--- gallery/views.py
+++ gallery/views.py
@@ -9,14 +9,18 @@ def about(request):
      return render(request, 'gallery/about.html')
 
 def home(request):
-
-    assets = Asset.objects.all()
+# all() возвращает хаос.
+# order_by('-created_at') сортирует по полю created_at.
+# Минус (-) означает "по убыванию" (DESC).
+    assets = Asset.objects.all().order_by('-created_at')
     context_data = {
-    'page_title': 'Главная Галерея',
-    'assets': assets, # Передаем реальный QuerySet (список)
+        'page_title': 'Главная Галерея',
+        'assets': assets,
 }
     return render(request, 'gallery/index.html', context_data)
 
-   
+def upload(request):
+     return render(request, 'gallery/upload.html')
+
 
  
